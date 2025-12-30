aa84839f90eb403a600c61355e8e8e0eea6a7c25
Frank Paul Silye <frankps@gmail.com>
2025-12-30 11:50:12 +0100

    Update and rename harbour-beginner-guide.ts to harbour-beginner-guide-nb.ts
    A quick translation of the first set of text strings to Norwegian bokmål.
    
    Please accept and thank you for making this app!

--- translations/harbour-beginner-guide-nb.ts
+++ translations/harbour-beginner-guide-nb.ts
@@ -5,7 +5,7 @@
     <name>CoverPage</name>
     <message>
         <source>My Cover</source>
-        <translation type="unfinished"></translation>
+        <translation>Mitt omslag</translation>
     </message>
 </context>
 <context>
@@ -13,7 +13,7 @@
     <message>
         <source>Loading...</source>
         <extracomment>Inside a loader</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Laster inn...</translation>
     </message>
 </context>
 <context>
@@ -21,43 +21,43 @@
     <message>
         <source>Loading...</source>
         <extracomment>Inside a loader</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Laster inn...</translation>
     </message>
     <message>
         <source>The requested page wasn&apos;t found, please try again later</source>
         <extracomment>Error message</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Den forespurte siden ble ikke funnet. Prøv igjen senere.</translation>
     </message>
     <message>
         <source>There was an error while loading the page, please try again later</source>
         <extracomment>Error message</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Det oppstod en feil under lasting av siden. Prøv igjen senere.</translation>
     </message>
     <message>
         <source>Settings</source>
         <extracomment>Pull down menu item</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Innstillinger</translation>
     </message>
     <message>
         <source>The requested app is not installed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Appen du forsøker å åpne er ikke installert.</translation>
     </message>
     <message>
         <source>This type of link is not supported.</source>
-        <translation type="unfinished"></translation>
+        <translation>Denne typen lenke støttes ikke.</translation>
     </message>
     <message>
         <source>Could not open the link.</source>
-        <translation type="unfinished"></translation>
+        <translation>Kunne ikke åpne lenken.</translation>
     </message>
     <message>
         <source>Tutorial</source>
         <extracomment>PUll down menu item</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Veiledning</translation>
     </message>
     <message>
         <source>It seems you don&apos;t have the Jolla store on your device.</source>
-        <translation type="unfinished"></translation>
+        <translation>Det ser ut til at du ikke har Jolla Store på enheten din.</translation>
     </message>
 </context>
 <context>
@@ -65,16 +65,16 @@
     <message>
         <source>Settings</source>
         <extracomment>Page title</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Innstillinger</translation>
     </message>
     <message>
         <source>Automatic</source>
         <extracomment>As in automatic language selection</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Automatisk</translation>
     </message>
     <message>
         <source>Language</source>
-        <translation type="unfinished"></translation>
+        <translation>Språk</translation>
     </message>
 </context>
 </TS>
